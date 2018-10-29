2394c816b417e5213b92a06cd4195509cff43542
joe <42183110+xiaomaofeng@users.noreply.github.com>
2018-10-29 18:37:23 +0800

    ADD FUNCTIONS

--- Function_notes/ListFiles.cpp
+++ Function_notes/ListFiles.cpp
@@ -4,6 +4,7 @@
 #include<io.h>
 #include<tchar.h>
 #include<windows.h>
+#include<iostream>
 //--------------------
 #include"Common.h"
 
@@ -37,7 +38,7 @@ void ListFiles(const string dir, vector<string>&Address, vector<string>sType)
 	{
 		if (findData.attrib&_A_SUBDIR)  //subdirectory
 		{
-			if (strcmp(findData.name, ".") == 0 || strcmp(findData.name, "..") == 0)
+			if (strcmp(findData.name, ".") == 0 || strcmp(findData.name, "..") == 0) //directory
 				continue;
 			DirNew = dir + "\\" + findData.name;
 			ListFiles(DirNew, Address, sType);
@@ -57,14 +58,54 @@ void ListFiles(const string dir, vector<string>&Address, vector<string>sType)
 	_findclose(handle); //关闭搜索句柄
 }
 
+void WinListFile(const string Dir, vector<string>Address, vector<string>stype)
+{
+	HANDLE handle;
+	WIN32_FIND_DATA findData;
+	string dirNew;
+	handle = FindFirstFile(dirNew.c_str(), &findData);
 
-//HANDLE handle;
-//WIN32_FIND_DATA findData;
-//handle = FindFirstFile(DirNew.c_str(), &findData);
-//if (handle == INVALID_HANDLE_VALUE)
-//return;
-//do
-//{
-//	if (findData.dwFileAttributes&)
-//
-//}
+	for (string Suffix : stype)
+	{
+		transform(Suffix.begin(), Suffix.end(), Suffix.begin(), ::tolower);//transform CASE and case
+	}
+
+	//----------------------------------------
+	//for example
+	/*
+	int nmatrix[10] = { 1,2,3,4,5,6,7,8,9,10 };   //In this example,the variable item holds the current value from the numbers array.
+	for (int a : nmatrix)
+	{
+		cout << "Count is:" << a << endl;
+	}
+	*/
+
+	dirNew = Dir + "\\*.*";
+
+	if (handle == INVALID_HANDLE_VALUE)
+		return;
+	do
+	{
+		bool bDirectory = (findData.dwFileAttributes&FILE_ATTRIBUTE_DIRECTORY) == FILE_ATTRIBUTE_DIRECTORY;
+		if (bDirectory)
+		{
+			if (strcmp(findData.cFileName, ".") == 0 || strcmp(findData.cFileName, "..") == 0)
+				continue;
+			dirNew = Dir + "\\" + findData.cFileName;
+			WinListFile(Dir, Address, stype);
+		}
+		else
+		{
+			for (string Suffix : stype)
+			{
+				if (GetSuffix(findData.cFileName) == Suffix)
+				{
+					string TempDirStr = Dir + "\\" + findData.cFileName;
+					Address.push_back(TempDirStr);
+				}
+			}
+		}
+
+	} while (FindNextFile(handle, &findData));
+	FindClose(handle);
+}
