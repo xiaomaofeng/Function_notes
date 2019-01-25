e854b27081bf75b1ded0911ce3fbbd9b99307807
joe <42183110+xiaomaofeng@users.noreply.github.com>
2019-01-25 19:37:12 +0800

    Create mpl_squares.py

---
+++ py/PythonIntroductionToPractice/data_visualization/mpl_squares.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares, linewidth=5)
+
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+#set tick marks's size
+plt.tick_params(axis='both', labelsize=14)
+
+plt.show()
