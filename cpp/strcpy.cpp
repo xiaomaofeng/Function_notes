7db5979b38b6285383a35e85ed860e2787177a8d
joe <42183110+xiaomaofeng@users.noreply.github.com>
2019-03-23 17:22:01 +0800

    Create strcpy.cpp

---
+++ cpp/strcpy.cpp
@@ -0,0 +1,8 @@
+#include<assert.h>
+char* strcpy(char *strDest, const char *strSrc)
+{
+	assert((strDest != NULL) && (strSrc != NULL));
+	char* address = strDest;
+	while ((*strDest++ = *strSrc++) != '\0');
+	return address;
+}
