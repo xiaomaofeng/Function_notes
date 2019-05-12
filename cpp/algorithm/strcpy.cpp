4f649af21231fe666d8e986eeefc8b0973394750
xiaomaofeng <42183110+xiaomaofeng@users.noreply.github.com>
2019-05-12 20:06:43 +0800

    Update strcpy.cpp

--- cpp/algorithm/strcpy.cpp
+++ cpp/algorithm/strcpy.cpp
@@ -12,7 +12,7 @@ char *strcpy(char *strDest, const char *strSrc)
 void *memcpy(void *memDest, const void *memSrc, size_t size)
 {
 	assert((memDest != NULL) && (memSrc != NULL));
-	char *addressSrc = (char *)memSrc;
+	char *addressSrc = (char *)memSrc;//memSrc's type is void*,so can't operate the pointer directly
 	char *addressDest = (char *)memDest;
 	if (memDest > (memSrc + size) || (memDest < memSrc))
 	{
